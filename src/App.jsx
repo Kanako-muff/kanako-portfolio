d04ca4673fc467089e9b5ad8ee8a7f528eec83ab
Kanako-muff <kanako.design00@gmail.com>
2023-10-05 22:30:52 -0700

    Three.js section done for now

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,27 @@ import Work from './components/work/Work'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Scrollup from './components/scrollup/Scrollup'
+import Images from './components/threejs/Images'
+import Slogan from './components/threejs/Slogan'
+import { Canvas } from '@react-three/fiber'
+import { ScrollControls, Scroll } from '@react-three/drei'
 
-function Images(){
-  return(
-    <div>
-      <Image url=""></Image>
-    </div>
-  )
-}
 
 function App() {
   return (
     <>
+      {/* ▼▽▼ Three.js Section ▼▽▼ */}
+      <Canvas>
+        <ScrollControls pages={2} dumping={3}>
+          <Scroll>
+            <Images />
+          </Scroll>
+          <Scroll html>
+            <Slogan />
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+      {/* ▲△▲ Three.js Section ▲△▲ */}
       <Header />
       <main className='main'>
         <Home />
