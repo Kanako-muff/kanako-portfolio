157095004476d1c113621f72abc2873396c6aceb
Kanako-muff <kanako.design00@gmail.com>
2023-10-11 19:03:07 -0700

    added Typed.js animation

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Typed from 'typed.js'
 
 const Data = () => {
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Front-end Developer.', 'Web Designer.', 'Graphic Designer.'],
+      typeSpeed: 50,
+      loop: true,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="home__data">
         <h1 className="home__title">Kanako Kawakami</h1>
-        <h3 className="home__subtitle">Visual Designer</h3>
+        <span className="home__subtitle" ref={el} />
         <p className="home__description">I transitioned from a web designer, where I used Photoshop/Illustrator and coded with HTML/CSS/JavaScript, to a full-stack developer through training at a Cornerstone College in Vancouver.</p>
         <a href="#contact" className="button button--flex">
             Say Hello&nbsp;
