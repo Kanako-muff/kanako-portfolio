ce0da4d39e41cafdb546d09da2f0a9428f59dc65
Kanako-muff <kanako.design00@gmail.com>
2023-12-19 00:41:09 -0800

    deleted night mode and Threejs

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,12 @@ import Work from './components/work/Work'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Scrollup from './components/scrollup/Scrollup'
-// import Images from './components/threejs/Images'
-// import Slogan from './components/threejs/Slogan'
-// import { Canvas } from '@react-three/fiber'
-// import { ScrollControls, Scroll } from '@react-three/drei'
 
 
 function App() {
   return (
     <>
-      {/* ▼▽▼ Three.js Section ▼▽▼ */}
-      {/* <Canvas>
-        <ScrollControls pages={2} dumping={3}>
-          <Scroll>
-            <Images />
-          </Scroll>
-          <Scroll html>
-            <Slogan />
-          </Scroll>
-        </ScrollControls>
-      </Canvas> */}
-      {/* ▲△▲ Three.js Section ▲△▲ */}
       <Header />
-      {/* <main className='main stars'> */}
         <Home />
         <div className='aboutSkills'>
           <About />
@@ -38,7 +21,6 @@ function App() {
         </div>
         <Work />
         <Contact />
-      {/* </main> */}
       <Footer />
       <Scrollup />
     </>
