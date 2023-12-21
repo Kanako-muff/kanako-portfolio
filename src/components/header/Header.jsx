b500b6068e672739d63b417eab67a515c3f441ef
Kanako-muff <kanako.design00@gmail.com>
2023-12-21 15:42:37 -0800

    added ENG/JPN button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -60,6 +60,15 @@ const Header = () => {
                             Contact
                         </a>
                     </li>
+                    <li className="nav__item">
+                        <div class="btn-group btn-group-sm" role="group" aria-label="Basic radio toggle button group">
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked />
+                            <label class="btn btn-outline-primary btn_color" for="btnradio1">ENG</label>
+
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" />
+                            <label class="btn btn-outline-primary btn_color" for="btnradio3">JPN</label>
+                        </div>
+                    </li>
                 </ul>
 
                 <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
